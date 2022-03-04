9b1365368dd09e1cb77e20ff6d82b79550b8057d
Maria <mfb171201@gmail.com>
2022-03-04 09:04:59 -0400

    done

--- migrations/versions/1c6003f49ba1_.py
+++ migrations/versions/1c6003f49ba1_.py
@@ -1,8 +1,8 @@
-"""adicionando tabela 'leads'
+"""empty message
 
-Revision ID: 59abf61fb646
+Revision ID: 1c6003f49ba1
 Revises: 
-Create Date: 2022-02-14 12:13:27.001927
+Create Date: 2022-03-04 09:02:22.887038
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '59abf61fb646'
+revision = '1c6003f49ba1'
 down_revision = None
 branch_labels = None
 depends_on = None
